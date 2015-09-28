1946a53db7eb78385a156a2782ed6c498380e545
jeaye <contact@jeaye.com>
2015-09-28 20:42:10 +0800

    Run el-get before package.el

--- init.el
+++ init.el
@@ -3,8 +3,8 @@
 
 (add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
 (require 'init-after)
-(require 'init-package)
 (require 'init-el-get)
+(require 'init-package)
 (require 'init-evil)
 (require 'init-color)
 (require 'init-indent)
