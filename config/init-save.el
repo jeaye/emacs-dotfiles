0cba2e225ae05e9ecafc98b3c4e7821516dc0ad6
jeaye <contact@jeaye.com>
2015-09-28 18:05:47 +0800

    Save cursor positions

--- config/init-save.el
+++ config/init-save.el
@@ -1,4 +1,9 @@
 ; No backups
 (setq make-backup-files nil)
 
+; Save the cursor position for each file
+(setq save-place-file "~/.emacs.d/saveplace")
+(setq-default save-place t)
+(require 'saveplace)
+
 (provide 'init-save)
