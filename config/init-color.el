2f2c7b92a36b5e2306283b5d81543e689a4016b1
jeaye <contact@jeaye.com>
2015-09-28 18:01:56 +0800

    Add solarized colorscheme

---
+++ config/init-color.el
@@ -0,0 +1,12 @@
+;; Solarized
+(require 'color-theme-sanityinc-solarized)
+(color-theme-sanityinc-solarized--define-theme dark)
+
+; Allow colors to work better in the terminal
+;(color-theme-approximate-on)
+
+; Colorize delimiters
+(require-package 'rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+(provide 'init-color)
