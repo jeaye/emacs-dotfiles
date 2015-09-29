acde39ac1850136bb2c35f35e35377d5ab241e31
jeaye <contact@jeaye.com>
2015-09-29 18:05:22 +0800

    Fix powerline from latest melpa

--- config/init-powerline.el
+++ config/init-powerline.el
@@ -1,5 +1,8 @@
-(add-to-list 'load-path "~/.emacs.d/packages/powerline-evil")
-;(require 'powerline-evil)
-;(powerline-evil-vim-color-theme)
+(use-package powerline
+  :config
+  (progn
+    (add-to-list 'load-path "~/.emacs.d/packages/powerline-evil")
+    (require 'powerline-evil)
+    (powerline-evil-vim-color-theme)))
 
 (provide 'init-powerline)
