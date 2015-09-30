2415260db9ac3cae1248b9f41d446600c7a83d34
jeaye <contact@jeaye.com>
2015-09-30 14:18:03 +0800

    Remove exit nags

--- config/init-save.el
+++ config/init-save.el
@@ -6,4 +6,8 @@
 (setq-default save-place t)
 (require 'saveplace)
 
+(defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
+  "Prevent annoying \"Active processes exist\" query when you quit Emacs."
+  (flet ((process-list ())) ad-do-it))
+
 (provide 'init-save)
