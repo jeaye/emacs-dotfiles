3e188ddd182386f8a8596c62170904d94f9a0a84
jeaye <contact@jeaye.com>
2015-09-30 14:37:45 +0800

    Work around a company + fci bug

--- config/init-autocomplete.el
+++ config/init-autocomplete.el
@@ -10,9 +10,23 @@
 (use-package company-ycmd
   :config
   (progn
-    (company-ycmd-setup)
     (setq company-backends (delete 'company-clang company-backends))
-    (global-company-mode)))
+    (setq company-idle-delay 0)
+    (global-company-mode)
+    (company-ycmd-setup)
+
+    ; Company + fci is fucked
+    ; https://github.com/company-mode/company-mode/issues/180
+    (defvar-local company-fci-mode-on-p nil)
+    (defun company-turn-off-fci (&rest ignore)
+      (when (boundp 'fci-mode)
+        (setq company-fci-mode-on-p fci-mode)
+        (when fci-mode (fci-mode -1))))
+    (defun company-maybe-turn-on-fci (&rest ignore)
+      (when company-fci-mode-on-p (fci-mode 1)))
+    (add-hook 'company-completion-started-hook 'company-turn-off-fci)
+    (add-hook 'company-completion-finished-hook 'company-maybe-turn-on-fci)
+    (add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci)))
 
 (use-package flycheck-ycmd
   :config
