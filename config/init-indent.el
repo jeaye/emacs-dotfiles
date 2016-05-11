7c5ef1637a3a0033dd9e77f854905beb4f62d988
jeaye <contact@jeaye.com>
2016-05-10 22:58:15 -0700

    Port indent configs to spacemacs

--- config/init-indent.el
+++ config/init-indent.el
@@ -2,9 +2,6 @@
 (setq-default default-tab-width 2
               indent-tabs-mode nil)
 
-; Indent after pressing return
-(define-key global-map (kbd "RET") 'newline-and-indent)
-
 (global-visual-line-mode)
 
 ; C++ indent style
@@ -12,13 +9,6 @@
       c-basic-offset 2)
 
 ; Color the gutter
-(use-package fill-column-indicator
-  :config
-  (progn
-    (setq fci-rule-column 80)
-    (define-globalized-minor-mode global-fci-mode
-                                  fci-mode
-                                  (lambda () (fci-mode 1)))
-    (global-fci-mode 1)))
+(add-hook 'after-change-major-mode-hook 'fci-mode)
 
 (provide 'init-indent)
