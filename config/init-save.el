52dd9ecbbfc025f2836dfaa4377f456a3081f913
jeaye <contact@jeaye.com>
2016-05-28 21:53:05 -0700

    Remove a warning about flet

--- config/init-save.el
+++ config/init-save.el
@@ -10,6 +10,6 @@
 
 (defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
   "Prevent annoying \"Active processes exist\" query when you quit Emacs."
-  (flet ((process-list ())) ad-do-it))
+  (cl-flet ((process-list ())) ad-do-it))
 
 (provide 'init-save)
